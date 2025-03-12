1d2454caf4694e0957eb75d4487400d11cbe62a7
Long <leminhlong11b8@gmail.com>
2025-03-12 15:15:20 +0700

    add clerk; sign in, sign up; layout

---
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+import { cn } from "@/src/lib/utils";
+
+interface SidebarItemProps {
+  icon: LucideIcon;
+  label: string;
+  href: string;
+}
+
+const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  //   const isActive =
+  //     (pathname === "/" && href === "/") ||
+  //     pathname === href ||
+  //     pathname?.startsWith(`${href}`);
+
+  const isActive = pathname === href;
+
+  const onClick = () => {
+    router.push(href);
+  };
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className={cn(
+        "flex items-center gap-x-2 pl-6 text-sm text-slate-500 font-[500] transition-all hover:bg-slate-300/20 hover:text-slate-600",
+        isActive &&
+          "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+      )}
+    >
+      <div className="flex items-center gap-x-2 py-4">
+        <Icon
+          size={22}
+          className={cn("text-slate-500", isActive && "text-sky-700")}
+        />
+        {label}
+      </div>
+
+      <div
+        className={cn(
+          "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+          isActive && "opacity-100"
+        )}
+      ></div>
+    </button>
+  );
+};
+
+export default SidebarItem;
